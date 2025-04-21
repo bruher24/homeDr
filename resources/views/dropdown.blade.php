0541794c97fd0ad1e866bca6a11c8802adc6a246
bruher24 <glushkovd2424@gmail.com>
2025-04-21 12:49:52 +0400

    Dockerized project \ Added basic blade templates

---
+++ resources/views/dropdown.blade.php
@@ -0,0 +1,7 @@
+<ul class="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 border-0 shadow w-220px" data-bs-theme="dark">
+    <li><a class="dropdown-item rounded-2 active" href="#">Action</a></li>
+    <li><a class="dropdown-item rounded-2" href="#">Another action</a></li>
+    <li><a class="dropdown-item rounded-2" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item rounded-2" href="#">Separated link</a></li>
+</ul>
